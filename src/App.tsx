9b7d44bdc8f3bbface0bcd135d1e0c9e56869165
Anastasia Dmitrovic <anastasia.dmitrovic@gmail.com>
2025-09-09 15:52:07 +0200

    Prazne stranice i App router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Rentals from './pages/Rentals';
+import RentalDetails from './pages/RentalDetails';
 
 function App() {
-  return <>Rentiva</>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/rentals' element={<Rentals />} />
+          <Route path='/rentals/:id' element={<RentalDetails />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/RentalDetails.tsx
@@ -0,0 +1,5 @@
+const RentalDetails = () => {
+  return <div>RentalDetails</div>;
+};
+
+export default RentalDetails;
